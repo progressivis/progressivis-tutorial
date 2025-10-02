815d2033e7afe9949ec216395b771a3ae8c6a1a1
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-10-02 22:42:01 +0200

    resync userguide1.5

--- tutorial/1-low-level/userguide1.5.py
+++ tutorial/1-low-level/userguide1.5.py
@@ -13,13 +13,19 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# # Dynamic Modification of a ProgressiVis Program
+#
+# This notebook shows how a running ProgressiVis program can be modified.
+# A simple program is created that loads a CSV file, compute the minumum, and prints a dot for each chunk.
+# Then, the program is modified; a max module is added after the CSV module and a slash is printed at each chunk.
+# Finally, the initial min module is deleted, triggering the deletion of the first print a **collateral dammage**.
+
 # %%
 from progressivis import (
     CSVLoader,
     Print,
     Min, Max,
-    Heatmap,
-    get_dataset,
 )
 
 # %%
@@ -27,16 +33,23 @@ import warnings
 warnings.filterwarnings("ignore")
 LARGE_TAXI_FILE = "https://www.aviz.fr/nyc-taxi/yellow_tripdata_2015-01.csv.bz2"
 
+# prints a dot at each chunk (run_step)
 def terse(x):
     print(".", end="", flush=True)
 
+# prints a slash at each chunk
 def terse2(x):
     print("/", end="", flush=True)
 
 
+# %% [markdown]
+# ## Creates the initial program
+#
+# The print module calls the function `terse` at each chunk, showing a dot when run.
+
 # %%
 csv = CSVLoader(LARGE_TAXI_FILE, usecols=['pickup_longitude', 'pickup_latitude'])
-m = Min()
+m = Min(name="min")
 prt = Print(proc=terse)
 m.input.table = csv.output.result
 prt.input.df = m.output.result
@@ -47,12 +60,32 @@ csv.scheduler.task_start()
 # %%
 csv.scheduler
 
+# %% [markdown]
+# # Adds a branch
+#
+# Compute the max and prints each chunk with a slash.
+# The branch is validated at the end of the `with` construct and, if valid, is run. Otherwise, an exception is raised and the initial program is not modified.
+
 # %%
 with csv.scheduler as dataflow:
-    M = Max()
+    M = Max(name="max")
     prt2 = Print(proc=terse2)
     M.input.table = csv.output.result
     prt2.input.df = M.output.result
+
+# %%
+csv.scheduler
+
+# %% [markdown]
+# # Removes the initial branch
+#
+# The min module is deleted, which also deletes the first print module.
+
+# %%
+with csv.scheduler as dataflow:
+    deps = dataflow.collateral_damage("min")
+    print("The collateral damage of deleting min is:", deps)
+    dataflow.delete_modules(*deps)
 
 # %%
 csv.scheduler
