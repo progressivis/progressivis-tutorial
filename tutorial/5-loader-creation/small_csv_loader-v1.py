a9daaebe2cac6d6904a5d5126e935545e6744168
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-10-05 22:43:30 +0200

    Simplify

--- tutorial/5-loader-creation/small_csv_loader-v1.py
+++ tutorial/5-loader-creation/small_csv_loader-v1.py
@@ -22,24 +22,7 @@ logger = logging.getLogger(__name__)
 @document
 @def_output("result", PTable, doc=RESULT_DOC)
 class SmallCSVLoaderV1(Module):
-    """
-    This module reads comma-separated values (csv) files progressively into a {{PTable}}.
-    Internally it uses :func:`pandas.read_csv`.
-    """
-
-    def __init__(
-            self,
-            filepath_or_buffer: Any,
-            fillvalues: dict[str, Any] | None = None,
-            **kwds: Any,
-    ) -> None:
-        r"""
-        Args:
-            filepath_or_buffer: str, path object or file-like object accepted by :func:`pandas.read_csv`
-            kwds: extra keyword args to be passed to :func:`pandas.read_csv` and :class:`Module <progressivis.core.Module>` superclass
-
-
-        """
+    def __init__(self, filepath_or_buffer: Any, **kwds: Any) -> None:
         if "index_col" in kwds:
             raise ProgressiveError("'index_col' parameter is not supported")
         super().__init__(**kwds)
@@ -53,42 +36,24 @@ class SmallCSVLoaderV1(Module):
             kwds.setdefault("chunksize", self.default_step_size)
         # Filter out the module keywords from the csv loader keywords
         csv_kwds: Dict[str, Any] = filter_kwds(kwds, pd.read_csv)
-        csv_kwds["index_col"] = False
-        self.filepath_or_buffer = filepath_or_buffer
-        self.parser: pd.io.parsers.readers.TextFileReader | None = None
-        self.csv_kwds = csv_kwds
+        self.parser = pd.read_csv(filepath_or_buffer, **csv_kwds)
         self._rows_read = 0
-        self._fillvalues = fillvalues
         self.result: PTable | None  # to help mypy
 
     def rows_read(self) -> int:
         return self._rows_read
 
     def is_data_input(self) -> bool:
-        # pylint: disable=no-self-use
-        "Return True if this module brings new data"
         return True
 
     def run_step(
         self, run_number: int, step_size: int, quantum: float
     ) -> ReturnRunStep:
         if step_size == 0:  # bug
-            logger.error("Received a step_size of 0")
             return self._return_run_step(self.state_ready, steps_run=0)
-        if self.parser is None:
-            try:
-                self.parser = pd.read_csv(self.filepath_or_buffer,
-                                          **self.csv_kwds)
-            except IOError as e:
-                logger.error("Cannot open file %s: %s",
-                             self.filepath_or_buffer, e)
-                self._return_run_step(self.state_zombie, steps_run=0)
-        logger.info("loading %d lines", step_size)
         try:
-            assert self.parser is not None
-            df: pd.DataFrame = self.parser.read(step_size)
+            df = self.parser.read(step_size)
         except StopIteration:
-            self.parser = None
             return self._return_run_step(self.state_zombie, steps_run=0)
         except ValueError:
             raise
@@ -97,20 +62,28 @@ class SmallCSVLoaderV1(Module):
             logger.error("Received 0 elements")
             return self._return_run_step(self.state_zombie, steps_run=0)
         self._rows_read += creates
-        logger.info("Loaded %d lines", self._rows_read)
-        force_valid_id_columns(df)
-        dshape = dshape_from_dataframe(df)
-        if self.result is None:
-            table_params: Dict[str, Any] = dict(name=self.name,
-                                                fillvalues=self._fillvalues)
+        force_valid_id_columns(df)  # fix column names
+        if self.result is None:  # create the PTable
+            table_params: Dict[str, Any] = dict(name=self.name)
             table_params["name"] = self.generate_table_name("table")
-            table_params["dshape"] = dshape
+            table_params["dshape"] = dshape_from_dataframe(df)  # infer types
             table_params["data"] = df
             table_params["create"] = True
             self.result = PTable(**table_params)
         else:
             self.result.append(df)
         return self._return_run_step(self.state_ready, steps_run=creates)
+        
+    def get_progress(self) -> Tuple[int, int]:
+        input_size = self.parser._input._input_size
+        if input_size == 0:
+            return (0, 0)
+        pos = self.parser._input._stream.tell()
+        length = len(self.result)
+        estimated_row_size = pos / length
+        estimated_size = int(input_size / estimated_row_size)
+        return (length, estimated_size)
+        
 
 
 def _test_1():
