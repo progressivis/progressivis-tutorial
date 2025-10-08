eaeac672931c6b5695fcea20bf9285bf9fb1c7bb
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-10-08 13:48:52 +0200

    Fix xargs in sync.sh and rename userguilde1.py to userguide1.0.py

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Creates the ipynb files and synchronize them all.
 
-find tutorial/1-low-level tutorial/4-visualization-creation -name '*.py'  -not -path "*/.ipynb_checkpoints/*" | xargs -i jupytext --set-formats ipynb,py {}
-find tutorial/1-low-level tutorial/4-visualization-creation -name '*.ipynb' -not -path "*/.ipynb_checkpoints/*" | xargs -i jupytext --sync {}
+find tutorial/1-low-level tutorial/4-visualization-creation -name '*.py'  -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupytext --set-formats ipynb,py {}
+find tutorial/1-low-level tutorial/4-visualization-creation -name '*.ipynb' -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupytext --sync {}
