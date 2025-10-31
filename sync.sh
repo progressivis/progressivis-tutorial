ff0d026c8c8a62e00ce711ba6a87b6b8f2ae36bc
Christian Poli <Christian.Poli@inria.fr>
2025-10-31 11:41:52 +0100

    fix

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@
 
 find tutorial/1-low-level tutorial/4-visualization-creation -name '*.py'  -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupytext --set-formats ipynb,py {}
 find tutorial/1-low-level tutorial/4-visualization-creation -name '*.ipynb' -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupytext --sync {}
-ls tutorial/2-progressibook/*.ipynb | xargs jupyter trust
+find tutorial/2-progressibook -name '*.ipynb' -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupyter trust {}
