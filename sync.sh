1308bcd8d51be99b0f15b4319a5990b8242d1196
Christian Poli <Christian.Poli@inria.fr>
2025-10-31 12:24:12 +0100

    adding traces for debug

--- sync.sh
+++ sync.sh
@@ -3,4 +3,6 @@
 
 find tutorial/1-low-level tutorial/4-visualization-creation -name '*.py'  -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupytext --set-formats ipynb,py {}
 find tutorial/1-low-level tutorial/4-visualization-creation -name '*.ipynb' -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupytext --sync {}
+echo "PWD $(/bin/pwd)"
+echo "LS $(ls tutorial/2-progressibook)"
 find tutorial/2-progressibook -name '*.ipynb' -not -path "*/.ipynb_checkpoints/*" | xargs -I{} jupyter trust {}
